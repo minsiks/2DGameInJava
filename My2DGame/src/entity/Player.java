af2aed9319acf03bb552e05139c9d1aecfb14bc1
minsiks <k_ms222@naver.com>
2023-05-18 09:09:07 +0900

    5.18 #3 DrawRectangleAddImage

---
+++ My2DGame/src/entity/Player.java
@@ -0,0 +1,60 @@
+package entity;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import javax.imageio.ImageIO;
+
+import main.GamePanel;
+import main.KeyHandler;
+
+public class Player extends Entity{
+
+	GamePanel gp;
+	KeyHandler keyH;
+	
+	public Player(GamePanel gp, KeyHandler keyH) {
+		this.gp = gp;
+		this.keyH = keyH;
+		
+		setDefaultValues();
+	}
+	public void setDefaultValues() {
+		
+		x = 100;
+		y = 100;
+		speed = 4;
+	}
+	public void getPlayerImage() {
+		try {
+			up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
+			up2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
+			down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
+			down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
+			left1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
+			left2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
+			right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
+			right2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
+			up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void update() {
+		if(keyH.upPressed == true) {
+			y-= speed;
+		}else if(keyH.downPressed == true) {
+			y+= speed;			
+		}else if(keyH.leftPressed == true) {
+			x-= speed;	
+		}else if(keyH.rightPressed == true) {
+			x+= speed;	
+		}
+	}
+	public void draw(Graphics2D g2) {
+		g2.setColor(Color.white);
+		
+		g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+	}
+}
