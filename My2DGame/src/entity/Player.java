fc114097b79322f2ec8cd09610466356412a5cee
minsiks <k_ms222@naver.com>
2023-05-23 10:11:30 +0900

    5.23 #7 CheckCollision & Create Object

--- My2DGame/src/entity/Player.java
+++ My2DGame/src/entity/Player.java
@@ -2,6 +2,7 @@ package entity;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 import javax.imageio.ImageIO;
@@ -22,6 +23,12 @@ public class Player extends Entity{
 		this.keyH = keyH;
 		this.screenX = gp.screenWidth/2 - (gp.tileSize/2);
 		this.screenY = gp.screenHeight/2 - (gp.tileSize/2);
+		
+		solidArea = new Rectangle();
+		solidArea.x = 8;
+		solidArea.y = 16;
+		solidArea.width = 32;
+		solidArea.height = 32;
 		
 		setDefaultValues();
 		getPlayerImage();
@@ -55,18 +62,27 @@ public class Player extends Entity{
 			
 			if(keyH.upPressed == true) {
 				direction = "up";
-				worldY-= speed;
 			}else if(keyH.downPressed == true) {
 				direction = "down";
-				worldY+= speed;			
 			}else if(keyH.leftPressed == true) {
 				direction = "left";
-				worldX-= speed;	
 			}else if(keyH.rightPressed == true) {
 				direction = "right";
-				worldX+= speed;	
 			}
 			
+			// CHECK TILE COLLISION
+			collisionOn = false;
+			gp.cChecker.checkTile(this);
+			
+			// IF COLLISION IS FALSE, PLAYER CAN MOVE
+			if(collisionOn == false) {
+				switch (direction) {
+				case "up": worldY -= speed; break;
+				case "down": worldY += speed; break;
+				case "left": worldX -= speed; break;
+				case "right": worldX += speed; break;
+				}
+			}
 			spriteCounter++;
 			if(spriteCounter > 12) {
 				if(spriteNum ==1) {
--- My2DGame/src/main/GamePanel.java
+++ My2DGame/src/main/GamePanel.java
@@ -8,6 +8,7 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 import entity.Player;
+import object.SuperObject;
 import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable{
@@ -34,7 +35,10 @@ public class GamePanel extends JPanel implements Runnable{
 	TileManager tileM = new TileManager(this);
 	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;	
+	public CollisionChecker cChecker = new CollisionChecker(this);
+	public AsserSetter aSetter = new AsserSetter(this);
 	public Player player = new Player(this, keyH);
+	public SuperObject obj[] = new SuperObject[10];
 	
 	public GamePanel() {
 		
@@ -45,6 +49,11 @@ public class GamePanel extends JPanel implements Runnable{
 		this.setFocusable(true);
 	}
 
+	public void setupGame() {
+		aSetter.setObject();
+		
+	}
+	
 	public void startGameThread() {
 		gameThread = new Thread(this);
 		gameThread.start();
@@ -113,13 +122,20 @@ public class GamePanel extends JPanel implements Runnable{
 		player.update();
 	}
 	public void paintComponent(Graphics g) {
-		
 		super.paintComponent(g);
-		
 		Graphics2D g2 = (Graphics2D) g;
 		
+		// TILE
 		tileM.draw(g2);
 		
+		// OBJECT
+		for(int i =0; i<obj.length; i++) {
+			if(obj[i] != null) {
+				obj[i].draw(g2, this);
+			}
+		}
+		
+		// PLAYER
 		player.draw(g2);
 		
 		g2.dispose();
